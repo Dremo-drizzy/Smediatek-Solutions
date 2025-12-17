52773bd3794362ca4b7c0077e93a108cfac39071
Dremo-drizzy <danielnwachukwu955@gmail.com>
2025-12-18 00:57:28 +0100

    Made changes to allow render see the po rt

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,9 @@ app.use("/api/training", trainingRoutes);
 
 // Bind server immediately
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
 // Connect to MongoDB asynchronously
 const connectDB = async () => {
